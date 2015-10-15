be787379750764e2e1492d03b23d4f2c36c5a6a5
James Conkling <james.lane.conkling@gmail.com>
2015-10-14 19:25:19 -0700

    prevent script from erroring out on pages other than home

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var landing = document.getElementById('landing');
 
-landing.addEventListener('click', function(){
-  this.style.opacity = 0;
-  setTimeout(function(){
-    this.style["z-index"] = 0;
-  }.bind(this), 600); // this must match fadeout in main.css
-});
+if(landing){
+  landing.addEventListener('click', function(){
+    this.style.opacity = 0;
+    setTimeout(function(){
+      this.style["z-index"] = 0;
+    }.bind(this), 600); // this must match fadeout in main.css
+  });
+}

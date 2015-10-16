f5dfa64114baa1325b424aa96a1599935a84e98a
James Conkling <james.lane.conkling@gmail.com>
2015-10-15 17:05:57 -0700

    prevent 'click' text from showing before splash image loads

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ var landing = document.getElementById('landing');
 
 if(landing){
   landing.addEventListener('click', function(){
-    this.style.opacity = 0;
+    if (this.classList)
+      this.classList.add('hidden');
+    else
+      this.className += ' ' + 'hidden';
+
     setTimeout(function(){
       this.style["z-index"] = 0;
     }.bind(this), 600); // this must match fadeout in main.css
